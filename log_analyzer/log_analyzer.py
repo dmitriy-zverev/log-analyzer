f660b0b12ac47faa6bd0dd57d13157d9c02f92cc
dmitriy-zverev <dimazverev716@gmail.com>
2025-12-05 17:24:42 +0300

    add creation of html files logic

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -3,6 +3,7 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 import gzip
+import json
 import os
 import re
 from collections import defaultdict
@@ -10,12 +11,32 @@ from datetime import datetime
 
 import structlog
 
+all_logs = []
+
+
+def capture_processor(_, __, event_dict):
+    all_logs.append(event_dict.copy())
+    return event_dict
+
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        capture_processor,
+        structlog.processors.JSONRenderer(),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+)
+
+
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "../reports",
     "LOG_DIR": "../log",
     "LOG_FILE_NAME": "nginx-access-ui",
     "TIME_FORMAT": "%d/%b/%Y:%H:%M:%S %z",
+    "REPORT_REPLACE_STRING": "$table_json",
 }
 
 logger = structlog.get_logger()
@@ -103,12 +124,43 @@ def get_group_stats(log_groups, total_logs):
         logger.info(log_url, **stats)
 
 
+def order_output_logs(all_logs):
+    return [
+        {
+            "url": log["event"],
+            "count": log["count"],
+            "count_perc": log["count_perc"],
+            "time_sum": log["time_sum"],
+            "time_avg": log["time_avg"],
+            "time_perc": log["time_perc"],
+            "time_max": log["time_max"],
+            "time_med": log["time_med"],
+        }
+        for log in all_logs
+    ]
+
+
+def write_to_report(all_logs_json, log_date):
+    report_template_name = os.path.join(config["REPORT_DIR"], "report.html")
+    with open(report_template_name, "r+") as f:
+        report_content = f.read()
+        updated_report_content = report_content.replace(config["REPORT_REPLACE_STRING"], all_logs_json)
+
+    new_report_filename = os.path.join(config["REPORT_DIR"], f"report-{log_date}.html")
+    with open(new_report_filename, "w") as f:
+        f.write(updated_report_content)
+
+    print(f"Created new log file: {new_report_filename}")
+
+
 def main():
-    log_files = get_log_files()[:1]
+    log_files = get_log_files()
     for log_file in log_files:
-        # log_date = log_file.split("log-")[-1]
-        log_content = [extract_data(log_line) for log_line in get_log_data(log_file)][:200]
+        log_content = [extract_data(log_line) for log_line in get_log_data(log_file)]
         get_group_stats(group_by_url(log_content), len(log_content))
+        write_to_report(json.dumps(order_output_logs(all_logs), default=str), log_file.split("log-")[-1])
+
+    print("Exiting...")
 
 
 if __name__ == "__main__":
